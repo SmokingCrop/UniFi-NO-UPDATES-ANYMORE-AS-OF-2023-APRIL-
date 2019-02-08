638f5c66763b081c93b6e187dbf551f56fdec2af
SmokingCrop <mahieutn@live.be>
2019-02-08 17:27:31 +0100

    Update install-unifi-pihole-English-test.sh

--- install-unifi-pihole-English-test.sh
+++ install-unifi-pihole-English-test.sh
@@ -3,7 +3,7 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\nEnter the Stable UniFi version of your choice (E.g: 5.9.29):${less}"
+echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\nEnter the Stable UniFi version of your choice (E.g: 5.10.12):${less}"
 read version
 echo -e "${Colour}\nChange your password:\nThe current password is raspberry\n${less}"
 passwd
