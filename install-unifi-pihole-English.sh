24d0769ea6377739c002c2a798e270bdd563dbde
SmokingCrop <mahieutn@live.be>
2019-03-02 21:51:34 +0100

    Added a default UniFi Controller version if nothing is typed in.

--- install-unifi-pihole-English.sh
+++ install-unifi-pihole-English.sh
@@ -3,14 +3,22 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\nEnter the Stable UniFi version of your choice (E.g: 5.10.19):${less}"
-read version
-echo -e "${Colour}\nChange your password:\nThe current password is raspberry\nYou can press enter to skip changing your password.\n${less}"
+echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
+read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.19: " version
+
+if [[ -z "$version" ]]; then
+	version='5.10.19'
+fi
+
+echo -e "${Colour}\nChange your password:\nThe current password is raspberry\n\nYou can press enter if you don't want to enter a new password\n${less}"
 passwd
+
 echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
+
 echo -e "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
+
 echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install the latest version of Oracle java 8.\n\n${less}"
 sudo apt-get install dirmngr -y;sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
 echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | sudo tee --append /etc/apt/sources.list
@@ -20,9 +28,12 @@ echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | 
 echo "debconf shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 echo "debconf shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
 sudo apt-get install oracle-java8-installer -y
+
 echo -e "${Colour}\n\nThe UniFi controller will be installed now.\n\n${less}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb; sudo apt-get install -f -y
+
 echo -e "${Colour}\n\nPi-hole will be installed now.\nThe initial configuration is interactive.\n\n${less}"
 curl -sSL https://install.pi-hole.net | bash
+
 echo -e "${Colour}\n\nOne more step is changing the password for the web interface of the Pi-hole.\n\n${less}"
 pihole -a -p
