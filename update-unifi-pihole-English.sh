5548cde88992a32ebf60b2d354e879fbc1b2740d
SmokingCrop <mahieutn@live.be>
2019-03-23 22:48:40 +0100

    Update update-unifi-pihole-English.sh

--- update-unifi-pihole-English.sh
+++ update-unifi-pihole-English.sh
@@ -3,8 +3,12 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo -e "${Colour}By using this script you will upgrade your system, the UniFi Controller and Pi-hole.\nWhich stable version of the UniFi controller do you wish to install? (E.g: 5.8.28)${less}"
-read version
+echo -e "${Colour}By using this script you will UPGRADE your system, the UniFi Controller and Pi-hole.\n${less}"
+read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.20: " version
+
+if [[ -z "$version" ]]; then
+	version='5.10.20'
+fi
 echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 echo -e "${Colour}\n\nThe UniFi controller (version $version) will now be downloaded.\n\n${less}"
