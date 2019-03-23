366df995983b52734866dacdf8dcf1da82f3a5c8
SmokingCrop <mahieutn@live.be>
2019-03-23 20:24:04 +0100

    Update install-unifi-pihole-English-test.sh

--- install-unifi-pihole-English-test.sh
+++ install-unifi-pihole-English-test.sh
@@ -4,10 +4,10 @@ Colour='\033[1;31m'
 less='\033[0m'
 
 echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
-read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.19: " version
+read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.20: " version
 
 if [[ -z "$version" ]]; then
-	version='5.10.19'
+	version='5.10.20'
 fi
 
 echo -e "${Colour}\nChange your password:\nThe current password is raspberry\n\nYou can press enter if you don't want to enter a new password\n${less}"
@@ -19,15 +19,8 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sud
 echo -e "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
-echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install the latest version of Oracle java 8.\n\n${less}"
-sudo apt-get install dirmngr -y;sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
-echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | sudo tee --append /etc/apt/sources.list
-echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | sudo tee --append /etc/apt/sources.list
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "debconf shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "debconf shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install the latest version of OpenJDK 8.\n\n${less}"
+sudo apt-get install openjdk-8-jre-headless -y
 
 echo -e "${Colour}\n\nThe UniFi controller will be installed now.\n\n${less}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb; sudo apt-get install -f -y
