d66c125ec324b29525b6ecb02e665b62c0ae903a
SmokingCrop <mahieutn@live.be>
2019-03-23 22:45:57 +0100

    Implementatie OpenJDK + haveged

--- install-unifi-pihole-Nederlands.sh
+++ install-unifi-pihole-Nederlands.sh
@@ -19,15 +19,11 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sud
 echo -e "${Kleur}\n\nNu wordt de UniFi Controller met versie $version gedownload.\n\n${loos}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
-echo -e "${Kleur}\n\nVooraleer de Controller geïnstalleerd wordt, wordt eerst de laatste versie van Oracle Java 8 geïnstalleerd.\n\n${loos}"
-sudo apt-get install dirmngr -y;sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
-echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | sudo tee --append /etc/apt/sources.list
-echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | sudo tee --append /etc/apt/sources.list
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "debconf shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "debconf shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+echo -e "${Colour}\n\nVooraleer de UniFi controller geïnstalleerd wordt, dient Java (OpenJDK 8) geïnstalleerd te worden.\n\n${less}"
+sudo apt-get install openjdk-8-jre-headless -y
+
+echo -e "${Colour}\n\nOm een oplossing te bieden voor de trage opstart van de UniFi controller door een specifiek probleem, wordt haveged geïnstalleerd.\n\n${less}"
+sudo apt-get install haveged -y
 
 echo -e "${Kleur}\n\nDe UniFi controller wordt nu geïnstalleerd.\n\n${loos}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb; sudo apt-get install -f -y
