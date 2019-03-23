674016fa7aeaf289b9f6ac93b8f675767791995d
SmokingCrop <mahieutn@live.be>
2019-03-23 22:49:50 +0100

    Update update-unifi-pihole-Nederlands.sh

--- update-unifi-pihole-Nederlands.sh
+++ update-unifi-pihole-Nederlands.sh
@@ -3,8 +3,13 @@
 Kleur='\033[1;31m'
 loos='\033[0m'
 
-echo -e "${Kleur}Met dit script wordt je systeem, de UniFi Controller en Pi-hole upgedatet.\nNaar welke versie van UniFi wens je up te graden? (bv. 5.8.28)${loos}"
-read version
+echo -e "${Kleur}Met dit script wordt je systeem, de UniFi Controller en Pi-hole upgedatet.\n${loos}"
+read -p "Typ de gewenste Stabiele UniFi Controller versie (bijv. 5.9.29) of druk op ENTER voor versie 5.10.20: " version
+
+if [[ -z "$version" ]]; then
+	version='5.10.20'
+fi
+
 echo -e "${Kleur}\n\nNu wordt het systeem up-to-date gebracht en worden de oude en niet gebruikte packages verwijderd.\n\n${loos}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 echo -e "${Kleur}\n\nDe UniFi Controller met versie $version wordt gedownload.\n\n${loos}"
