5d6c9cc66ceca58aa1ea1be511876cd2b60da7a8
SmokingCrop <mahieutn@live.be>
2019-04-02 22:53:01 +0200

    Update install-unifi-pihole-English-test.sh

--- install-unifi-pihole-English-test.sh
+++ install-unifi-pihole-English-test.sh
@@ -2,7 +2,6 @@
 
 Colour='\033[1;31m'
 less='\033[0m'
-pihole=true
 
 echo -e "${Colour}By using this script, you'll adjust the password, update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
 read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.20: " version
@@ -29,7 +28,7 @@ sudo apt-get install haveged -y
 echo -e "${Colour}\n\nThe UniFi controller will be installed now.\n\n${less}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb; sudo apt-get install -f -y
 
-if [ "$pihole" = true ] ; then
+if [[ -z "$1" ]] ; then
 echo -e "${Colour}\n\nPi-hole will be installed now.\nThe initial configuration is interactive.\n\n${less}"
 curl -sSL https://install.pi-hole.net | bash
 
