553df6f5f1d3e3867d12f2bd0a29eed1a07d1aec
SmokingCrop <mahieutn@live.be>
2019-06-08 10:37:46 +0200

    Update install-unifi-pihole-Nederlands.sh

--- install-unifi-pihole-Nederlands.sh
+++ install-unifi-pihole-Nederlands.sh
@@ -4,10 +4,10 @@ Kleur='\033[1;31m'
 loos='\033[0m'
 
 echo -e "${Kleur}Met dit script passen we het wachtwoord aan, wordt het systeem up-to-date gebracht, installeren we de UniFi controller met versie van jouw keuze en wordt Pi-hole geïnstalleerd.\nDruk op CTRL+C om het script te stoppen.\n${loos}"
-read -p "Typ de gewenste Stabiele UniFi Controller versie (bijv. 5.9.29) of druk op ENTER voor versie 5.10.21: " version
+read -p "Typ de gewenste Stabiele UniFi Controller versie (bijv. 5.9.29) of druk op ENTER voor versie 5.10.24: " version
 
 if [[ -z "$version" ]]; then
-	version='5.10.21'
+	version='5.10.24'
 fi
 
 echo -e "${Kleur}\nVerander je wachtwoord:\nHet standaard wachtwoord is raspberry\nJe kan op enter drukken om dit over te slaan\n${loos}"
