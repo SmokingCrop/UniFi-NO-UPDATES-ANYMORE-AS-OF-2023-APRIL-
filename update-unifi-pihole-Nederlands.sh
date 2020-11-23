c241d5c87a42ac90195aad876af6e2dcf9d6c874
SmokingCrop <mahieutn@live.be>
2020-11-23 22:19:56 +0100

    6.0.36

--- update-unifi-pihole-Nederlands.sh
+++ update-unifi-pihole-Nederlands.sh
@@ -4,10 +4,10 @@ Kleur='\033[1;31m'
 loos='\033[0m'
 
 echo -e "${Kleur}Met dit script wordt je systeem, de UniFi Controller en Pi-hole upgedatet.\n${loos}"
-read -p "Typ de gewenste Stabiele UniFi Controller versie (bijv. 5.11.50) of druk op ENTER voor versie 6.0.28: " version
+read -p "Typ de gewenste Stabiele UniFi Controller versie (bijv. 5.11.50) of druk op ENTER voor versie 6.0.36: " version
 
 if [[ -z "$version" ]]; then
-	version='6.0.28'
+	version='6.0.36'
 fi
 
 echo -e "${Kleur}\n\nNu wordt het systeem up-to-date gebracht en worden de oude en niet gebruikte packages verwijderd.\n\n${loos}"
