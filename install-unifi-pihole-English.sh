145ce827379b0f74f99f4a5fadedc8c82d2a068b
SmokingCrop <mahieutn@live.be>
2021-11-12 20:12:40 +0100

    Compatability fixes for Raspbian 11 (Bullseye).

--- install-unifi-pihole-English.sh
+++ install-unifi-pihole-English.sh
@@ -13,14 +13,17 @@ fi
 echo -e "${Colour}\nChange your default password:\nThe default password for the user pi is raspberry\n\nYou can press enter if you don't want to update your password\n${less}"
 passwd
 
+echo -e "${Colour}\n\nAdding the Raspbian Stretch sources.list for MongoDB compatability.\n\n${less}"
+echo 'deb http://archive.raspbian.org/raspbian stretch main contrib non-free rpi' | sudo tee /etc/apt/sources.list.d/raspbian_stretch_for_mongodb.list
+
 echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt-get autoclean -y
 
 echo -e "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
 wget http://dl.ui.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
-echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install OpenJDK 8.\n\n${less}"
-sudo apt install openjdk-8-jre-headless -y
+echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install OpenJDK 8 (Java), jsvc and libcommons-daemon-java which are required to install the UniFi controller.\n\n${less}"
+sudo apt install openjdk-8-jre-headless jsvc libcommons-daemon-java -y
 
 echo -e "${Colour}\n\nIn order to fix an issue which can cause a slow start for the UniFi controller, haveged is installed.\n\n${less}"
 sudo apt install haveged -y
@@ -35,3 +38,8 @@ curl -sSL https://install.pi-hole.net | bash
 echo -e "${Colour}\n\nOne more step is changing the password for the web interface of the Pi-hole.\n\n${less}"
 pihole -a -p
 fi
+
+echo -e "${Colour}\n\nTo finish the installation, a reboot is required. Starting a reboot in 3 seconds.\n\n${less}"
+sleep 3
+echo -e "${Colour}\nRestarting the Raspberry Pi now.\n${less}"
+sudo reboot now
