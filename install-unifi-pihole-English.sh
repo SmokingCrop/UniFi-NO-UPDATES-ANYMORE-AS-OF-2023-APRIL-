d5711d69a6ce49df91ad07ee19096f1695f97920
SmokingCrop <mahieutn@live.be>
2023-04-03 01:13:20 +0200

    No longer works on RPI4.
    No longer works on RPI4 and don't care enough anymore to try and fix it. I don't even have a RPI4 and haven't used the script for myself for over a year, probably even close to 3 years...

--- install-unifi-pihole-English.sh
+++ install-unifi-pihole-English.sh
@@ -4,15 +4,14 @@ Colour='\033[1;31m'
 less='\033[0m'
 requiredver='7.3.83'
 
+echo -e "${Colour}THIS NO LONGER WORKS ON A FULLY UPDATED RASPBERRY PI 4. Please try this script instead: https://community.ui.com/questions/UniFi-Installation-Scripts-or-UniFi-Easy-Update-Script-or-UniFi-Lets-Encrypt-or-UniFi-Easy-Encrypt-/ccbc7530-dd61-40a7-82ec-22b17f027776?page=1\n\n${less}"
+
 echo -e "${Colour}By using this script, you'll update the system, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
 read -p "Please enter a STABLE version of your choice (e.g: 7.3.76) or press enter for the latest stable version 7.3.83: " version
 
 if [[ -z "$version" ]]; then
 	version='7.3.83'
 fi
-
-echo -e "${Colour}\n\nMaking sure the Pi will not run 64 bit kernel..\n\n${less}"
-sudo sh -c 'echo "arm_64bit=0" >> /boot/config.txt'
 
 echo -e "${Colour}\n\nAdding the Raspbian Stretch sources.list for MongoDB compatability.\n\n${less}"
 echo 'deb http://archive.raspbian.org/raspbian stretch main contrib non-free rpi' | sudo tee /etc/apt/sources.list.d/raspbian_stretch_for_mongodb.list
